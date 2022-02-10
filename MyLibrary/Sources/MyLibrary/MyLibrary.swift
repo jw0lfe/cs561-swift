5e5cd2d0b7782803fb4687c8f935f51fce543900
jw0lfe <97068183+jw0lfe@users.noreply.github.com>
2022-02-09 20:55:00 -0800

    Connecting to EC2 server and tests

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -9,28 +9,35 @@ public class MyLibrary {
         self.weatherService = weatherService ?? WeatherServiceImpl()
     }
 
-    public func isLucky(_ number: Int, completion: @escaping (Bool?) -> Void) {
-        // Check the simple case first: 3, 5 and 8 are automatically lucky.
-        if number == 3 || number == 5 || number == 8 {
-            completion(true)
-            return
-        }
-
+    public func getWeather(completion: @escaping (Int?) -> Void) {
+                
         // Fetch the current weather from the backend.
         // If the current temperature, in Farenheit, contains an 8, then that's lucky.
         weatherService.getTemperature { response in
             switch response {
             case let .failure(error):
                 print(error)
-                completion(nil)
+                completion(0)
 
             case let .success(temperature):
-                if self.contains(temperature, "8") {
-                    completion(true)
-                } else {
-                    let isLuckyNumber = self.contains(temperature, "8")
-                    completion(isLuckyNumber)
-                }
+                print(temperature)
+                completion(temperature)
+            }
+        }
+    }
+
+    public func getHello(completion: @escaping (String?) -> Void) {
+                
+        // Fetch the current weather from the backend.
+        // If the current temperature, in Farenheit, contains an 8, then that's lucky.
+        weatherService.getGreeting { response in
+            switch response {
+            case let .failure(error):
+                completion("failed")
+
+            case let .success(greeting):
+                print(greeting)
+                completion(greeting)
             }
         }
     }
@@ -38,7 +45,7 @@ public class MyLibrary {
     /// Sample usage:
     ///   `contains(558, "8")` would return `true` because 588 contains 8.
     ///   `contains(557, "8")` would return `false` because 577 does not contain 8.
-    private func contains(_ lhs: Int, _ rhs: Character) -> Bool {
-        return String(lhs).contains(rhs)
-    }
+    // private func contains(_ lhs: Int, _ rhs: Character) -> Bool {
+    //     return String(lhs).contains(rhs)
+    // }
 }
--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -1,24 +1,100 @@
 import Alamofire
+import Foundation
 
 public protocol WeatherService {
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void)
+    func getGreeting(completion: @escaping (_ response: Result<String /* Greeting */, Error>) -> Void)
+    func getToken(completion: @escaping (_ token: String) -> Void)
 }
 
 class WeatherServiceImpl: WeatherService {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
+    let urlWeather = "http://ec2-52-12-191-147.us-west-2.compute.amazonaws.com:3000/v1/weather"
+    let urlHello = "http://ec2-52-12-191-147.us-west-2.compute.amazonaws.com:3000/v1/hello"
+    let urlAuth = "http://ec2-52-12-191-147.us-west-2.compute.amazonaws.com:3000/v1/auth"
+    //let urlWeather = "http://localhost:3000/v1/weather"
+    //let urlHello = "http://localhost:3000/v1/hello"
+    //let urlAuth = "http://localhost:3000/v1/auth"
 
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
-        AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
+        self.getToken(completion: {token in let headers : HTTPHeaders = ["Authorization": "Bearer  \(token)"]
+            AF.request(self.urlWeather, method: .get, headers: headers).validate(statusCode: 200..<500).responseDecodable(of: Weather.self) { response in
+                switch response.result {
+                case let .success(weather):
+                    let temperature = weather.main.temp
+                    let temperatureAsInteger = Int(temperature)
+                    print(temperatureAsInteger)
+                    completion(.success(temperatureAsInteger))
+
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }
+        })
+    }
+
+    func getGreeting(completion: @escaping (_ response: Result<String /* Greeting */, Error>) -> Void) {
+        self.getToken(completion: {token in let headers : HTTPHeaders = ["Authorization": "Bearer \(token)"]
+            AF.request(self.urlHello, method: .get, headers: headers).validate(statusCode: 200..<500).responseDecodable(of: Greeting.self) { response in
+                switch response.result {
+                case let .success(greeting):
+                    let hello = greeting.greeting
+                    print(hello)
+                    completion(.success(hello))
+
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }
+        })
+    }
+
+    func getToken(completion: @escaping (_ token: String) -> Void) {
+        let parameters: Parameters = [
+            "username": "jabob",
+            "password": "D0nChaKn0w"
+        ]
+
+        AF.request(self.urlAuth, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate(statusCode: 200..<500).responseDecodable(of: Token.self) { response in
             switch response.result {
-            case let .success(weather):
-                let temperature = weather.main.temp
-                let temperatureAsInteger = Int(temperature)
-                completion(.success(temperatureAsInteger))
+            case let .success(accesstoken):
+                let token = accesstoken.accesstoken
+                completion(token)
 
             case let .failure(error):
-                completion(.failure(error))
+                completion("error")
             }
         }
+
+        // AF.request(self.urlAuth, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate(statusCode: 200..<500).responseJSON { response in
+        //     switch response.result {
+        //         case let .success(value):
+        //             if let json = try? JSONSerialization.jsonObject(with: response.data!, options: []) as! [String: Any] {
+
+        //                 if let token = json["accesstoken"] as? String {
+        //                     completion(token)
+        //                 }
+        //             }
+
+        //         case let .failure(error):
+        //             completion("error")
+        //     }
+        // }
+
+        // AF.request(self.urlAuth, method: .post, parameters: parameters).validate(statusCode: 200..<500).responseDecodable(of: Token.self) { AFdata in
+        //     //https://www.hackingwithswift.com/example-code/system/how-to-parse-json-using-jsonserialization
+        //     do {
+        //         if let json = try JSONSerialization.jsonObject(with: AFdata.data!) as? [String: Any] {
+        //             if let token = json["accesstoken"] as? String {
+        //                 let tokens = String(token)
+        //                 print(tokens)
+        //                 completion(tokens)
+        //             }
+        //         }
+        //     } catch {
+        //         print("Error")
+        //         completion("")
+        //     }
+        // }
     }
 }
 
@@ -28,4 +104,12 @@ private struct Weather: Decodable {
     struct Main: Decodable {
         let temp: Double
     }
+}
+
+private struct Greeting: Decodable {
+    let greeting: String
+}
+
+private struct Token: Decodable {
+    let accesstoken: String
 }
